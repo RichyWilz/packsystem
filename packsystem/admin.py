420fd5d9d7172042581e3d6abae6ed1a3a16c7fb
Kayiira Richard <richardwilsonevarist@gmail.com>
2023-11-18 12:34:49 +0300

    Please note all the html is in templates folder in the package folder, css located in the static folder, images in images inside static. there seems to be repetition between the static and staticfiles folders that I'll troubleshoot

--- packsystem/admin.py
+++ packsystem/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import PackageSystemUser
 
-# Register your models here.
+admin.site.site_header = 'PACKAGE SYSTEM ADMINISTRATION'
+admin.site.register(PackageSystemUser)
